6405f02766d61efa761358d6004d9ce32776cec7
David Sally <davidcsally@gmail.com>
2020-08-22 20:36:26 -0700

    Add eslint + typescript + lint --fix

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
-import kontra from 'kontra';
+import kontra from 'kontra'
 
-let { init, Sprite, GameLoop } = kontra;
+const { init, Sprite, GameLoop } = kontra
 
-let { canvas } = init();
+const { canvas } = init()
 
-let sprite = Sprite({
+const sprite = Sprite({
   x: 100, // starting x,y position of the sprite
   y: 80,
   color: 'red', // fill color of the sprite rectangle
   width: 20, // width and height of the sprite rectangle
   height: 40,
   dx: 2, // move the sprite 2px to the right every frame
-});
+})
 
-let loop = GameLoop({
+const loop = GameLoop({
   // create the main game loop
-  update: function () {
+  update() {
     // update the game state
-    sprite.update();
+    sprite.update()
 
     // wrap the sprites position when it reaches
     // the edge of the screen
     if (sprite.x > canvas.width) {
-      sprite.x = -sprite.width;
+      sprite.x = -sprite.width
     }
   },
-  render: function () {
+  render() {
     // render the game state
-    sprite.render();
+    sprite.render()
   },
-});
+})
 
-loop.start(); // start the game
+loop.start() // start the game
